fdf6a04e939fb06b251d62793420d07641e0545f
astridite <astridite@gmail.com>
2020-09-15 16:05:01 +0200

    removed dashboard

--- RainbowPages/dashboard/sheet.R
+++
@@ -1,8 +0,0 @@
-library(googlesheets4)
-library(tidyverse)
-library(janitor)
-
-#gs4_auth()
-responses <- read_sheet("https://docs.google.com/spreadsheets/d/1JPwEsgts0ryTBNsl4AQk6Lz4rEotANLlJSPwioO6PD0/edit#gid=1555770637")
-
-
