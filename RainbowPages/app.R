7820ed35e32360a22158419683a3fc9c8211fc1e
astridite <astridite@gmail.com>
2020-09-16 17:29:29 +0200

    overhaulled layout and wrangled data

--- RainbowPages/app.R
+++ RainbowPages/app.R
@@ -2,52 +2,67 @@ library(shiny)
 library(shinythemes)
 library(shinyWidgets)
 library(DT)
+library(dplyr)
 library(magrittr)
 library(leaflet)
-source("sheet.R")
+library(RCurl)
+library(stringr)
 source("data_manip.R")
+
+curated_data <- read.csv(text=getURL("https://raw.githubusercontent.com/astridite/rainbow_pages/dev/RainbowPages/curated_data.csv")) 
 
 ui <-  fluidPage(theme=shinytheme("yeti"),
   navbarPage("Rainbow Pages Cape Town",
-                 tabPanel("Welcome!", icon=icon("heart"),
+                 # tabPanel("Welcome!", icon=icon("heart"),
+                 #          fluidPage(
+                 #            fluidRow(column(
+                 #              tags$h2(welcome_text), width = 12)),
+                 #            fluidRow(column(
+                 #              tags$br(),
+                 #              tags$br(),
+                 #              tags$h4(welcome_para1, align="justify"),
+                 #              tags$p("If you would like to see yourself or your business/organisation listed here, please fill out ",
+                 #                tags$a(href="https://docs.google.com/forms/d/1Ot4rNE-hQilKH7DTdWoMvTYTh5BX1__uoBhdpm91ZwU/edit", "this form.")),
+                 #              tags$p(welcome_para2, align="justify"),
+                 #              width=6)))),
+                 tabPanel("Browse",
                           fluidPage(
                             fluidRow(column(
-                              tags$h2(welcome_text), width = 12)),
-                            fluidRow(column(
+                              tags$h2(welcome_text), 
                               tags$br(),
                               tags$br(),
-                              tags$h4(welcome_para1, align="justify"),
-                              tags$p("If you would like to see yourself or your business/organisation listed here, please fill out ",
-                                tags$a(href="https://docs.google.com/forms/d/1Ot4rNE-hQilKH7DTdWoMvTYTh5BX1__uoBhdpm91ZwU/edit", "this form.")),
-                              tags$p(welcome_para2, align="justify"),
-                              width=6)))), 
-                 tabPanel("Businesses", icon=icon("briefcase"), 
-                          fluidPage(
-                            fluidRow(column(tags$img(src='www/banner.jpg'), width=12),
-                                     column(tags$h3(business_text1), width=12),
-                                     column(tags$br(), width=12),
-                                     column(DT::dataTableOutput(outputId = "busi"), width=12)))),
-                 tabPanel("Organisations", icon=icon("hands-helping"), 
-                          fluidPage(
-                            fluidRow(column(tags$img(src='www/banner.jpg'), width=12),
-                                     column(tags$h3(organisation_text1), width=12),
-                                     column(tags$br(), width=12),
-                                     column(DT::dataTableOutput("orgs"), width=12)))),
-                 tabPanel("Individuals",  icon=icon("address-book"), 
-                          fluidPage(
-                            fluidRow(column(tags$img(src='www/banner.jpg'), width=12),
-                                     column(tags$h3(individual_text1), width=12),
-                                     column(tags$br(), width=12),
-                                     column(DT::dataTableOutput("indiv"), width=12)))), 
-                 tabPanel("Map", icon=icon("map-marked-alt"), 
-                          fluidPage(
-                            fluidRow(column(tags$img(src='www/banner.jpg'), width=12),
-                                     column(tags$h3(map_text1), width=12), 
-                                     column(tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
-                                            leafletOutput("map"), width=12)))), 
-                 tabPanel("Network", icon=icon("draw-polygon"),
-                          fluidPage(
-                            fluidRow(column(tags$h3(work_in_progress), width=12)))),
+                              width = 12)
+                              ),
+                            sidebarLayout(
+                              sidebarPanel(
+                                tags$br(),
+                                style=HTML("background-color: #ffffff;", 
+                                           "border-color: #ffffff;"),
+                                checkboxGroupInput('type', 
+                                                   label=tags$h4("Filter by Type"),
+                                                   selected=levels(factor(curated_data$class)),
+                                                   choices =levels(factor(curated_data$class)),
+                                                   inline = F),
+                                checkboxGroupInput('location', 
+                                            label=tags$h4("Filter by Location"),
+                                            selected=levels(factor(curated_data$location)),
+                                            choices =levels(factor(curated_data$location)),
+                                            inline = F,
+                                            width = '100%'),
+                                width=2),
+                            mainPanel(
+                              (DT::dataTableOutput(outputId = "browse")))
+                          ))),
+
+                 # tabPanel("Map", icon=icon("map-marked-alt"), 
+                 #          fluidPage(
+                 #            fluidRow(column(tags$img(src='www/banner.jpg'), width=12),
+                 #                     column(tags$h3(map_text1), width=12), 
+                 #                     column(tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+                 #                            leafletOutput("map"), width=12)))), 
+                 # tabPanel("Network", icon=icon("draw-polygon"),
+                 #          fluidPage(
+                 #            fluidRow(column(tags$h3(work_in_progress), width=12)))),
                  tabPanel("Resources", icon=icon("info-circle"), 
                           fluidPage(
                             fluidRow(column(tags$h3(resources_text1,
@@ -64,43 +79,31 @@ ui <-  fluidPage(theme=shinytheme("yeti"),
                             fluidRow(column(tags$h3(work_in_progress), width=12))))),
   
   tags$style(HTML("a {color: #eb34c0}"))
-    
-  
 )
 
 server <- function(input, output) {
-    output$busi <- DT::renderDataTable({
-      datatable(busi,
-                filter="top",
-                options=list(
-                  columnDefs = list(list(visible=FALSE, targets=c(2,6,10))),
-                  paging=F))%>% 
-        formatStyle('colours', target = 'row', backgroundColor = styleEqual(c(1:8), hex),
-                    fontSize = '90%')})
-    output$orgs <- DT::renderDataTable({
-         datatable(orgs, 
-                   filter="top",
-                   options=list(
-                     columnDefs = list(list(visible=FALSE, targets=c(2,6,10))),
-                     paging=F))%>% 
-        formatStyle('colours', target = 'row', backgroundColor = styleEqual(c(1:8), hex),
-                    fontSize = '90%') 
-        })
-    output$indiv <- DT::renderDataTable({
-        datatable(indiv, 
-                  filter="top",
-                  options=list(columnDefs = list(list(visible=FALSE, targets=c(2,6,9))), 
-                               paging=F)) %>%
-            formatStyle('colours',
-                        target = 'row',
-                        backgroundColor = styleEqual(c(1:8), hex)) 
-        })
-    output$map <- renderLeaflet({
-        leaflet(data = markers) %>%
-        addTiles() %>%
-        setView(lng = 18.495678, lat = -33.939157, zoom = 12) %>%
-        addMarkers(lng = ~lon, lat = ~lat, popup = ~as.character(label), clusterOptions = markerClusterOptions())
-        })
+  data <- reactive(
+    curated_data %>% 
+      dplyr::filter(class %in% input$type) %>% 
+      dplyr::filter(location %in% input$location) 
+  )
+          
+  output$browse <- renderDataTable({
+    datatable(data(),
+              rownames = F,
+              options=list(
+              columnDefs = list(list(visible=FALSE, targets=c(1,5,6,8,9,11))),
+              paging=F)) %>% 
+      formatStyle('colours', 
+                  target = 'row', 
+                  backgroundColor = styleEqual(c(1:8), hex))
+  })
+    # output$map <- renderLeaflet({
+    #     leaflet(data = markers) %>%
+    #     addTiles() %>%
+    #     setView(lng = 18.495678, lat = -33.939157, zoom = 12) %>%
+    #     addMarkers(lng = ~lon, lat = ~lat, popup = ~as.character(label), clusterOptions = markerClusterOptions())
+    #     })
     
 }
 
--- RainbowPages/data_manip.R
+++ RainbowPages/data_manip.R
@@ -1,106 +1,70 @@
 library(tmaptools)
-
-curated <- responses %>% 
-  row_to_names(1) %>% 
-  select(id, contact_person, email, website, business, individual, 
-         organisation, class, address, location, sector,
-         twitter, instagram, facebook, linkedin) %>% 
-  arrange(id) %>% 
-  rowwise() %>% 
-  mutate(social_media = paste(ifelse(is.na(facebook), "", paste("Facebook: ", facebook, " ")),
-                              ifelse(is.na(instagram), "", paste("Instagram: ", instagram, " ")),
-                              ifelse(is.na(twitter), "", paste("Twitter: ", twitter, " ")),
-                              ifelse(is.na(linkedin), "", paste("Linkedin: ", linkedin, " "))))
-                              
-
-busi <- curated %>% 
-  filter(class=="A business") %>% 
-  select(-organisation, -individual) %>% 
-  rename(detail = 'business') %>% 
-  rename(business='id') %>% 
-  mutate(colours = rep_len(1:8, length.out = (nrow(.)))) %>% 
-  mutate(email = str_replace(email, '@', " @")) %>% 
-  mutate(sector = str_replace(sector, '@', " @"))
-
-orgs <- curated %>% 
-  filter(class=="A NGO/NPO/Community Organisation") %>% 
-  select(-business, -individual) %>% 
-  rename(detail = 'organisation') %>% 
-  rename(organisation='id') %>% 
-  mutate(colours = rep_len(1:8, length.out = (nrow(.))))
-
-indiv <- curated %>% 
-  filter(class=="An individual") %>% 
-  mutate(address=NA) %>% 
-  select(-business, -organisation, -address) %>% 
-  rename(detail = 'individual') %>% 
-  rename(individual='id') %>% 
-  mutate(colours = rep_len(1:8, length.out = (nrow(.)))) 
-
-# Only add marker locations where we have an actual address
-locations <- curated %>%
-  filter(!is.na(address)) %>%
-  select(address)
-
-# Geocode addresses
-coords <- map_dfr(locations, geocode_OSM)
-
-# Create a dataframe of marker coordinates and labels
-markers <- curated %>%
-  inner_join(coords, by = c('address' = 'query')) %>%
-  mutate(label = paste(sep = "<br/>",
-                       sprintf("<b>%s</b>", id),
-                       business,
-                       address)) %>%
-  select(lat, lon, label)
-
-hex <- c("#FF6663", "#FEB144", "#FDFD97", "#9EE09E", "#9EC1CF", "#995bc7", "#CC99C9", "#935218")
-
-library(tmaptools)
-
-curated <- responses %>% 
-  row_to_names(1) %>% 
-  select(id, contact_person, email, social_media, business, individual, 
-         organisation, class, address, location, sector) %>% 
-  arrange(id) 
-
-busi <- curated %>% 
-  filter(class=="A business") %>% 
-  select(-organisation, -individual) %>% 
-  rename(detail = 'business') %>% 
-  rename(business='id') %>% 
-  mutate(colours = rep_len(1:8, length.out = (nrow(.))))
-
-orgs <- curated %>% 
-  filter(class=="A NGO/NPO/Community Organisation") %>% 
-  select(-business, -individual) %>% 
-  rename(detail = 'organisation') %>% 
-  rename(organisation='id') %>% 
-  mutate(colours = rep_len(1:8, length.out = (nrow(.))))
-
-indiv <- curated %>% 
-  filter(class=="An individual") %>% 
-  select(-business, -organisation, -address) %>% 
-  rename(detail = 'individual') %>% 
-  rename(individual='id') %>% 
-  mutate(colours = rep_len(1:8, length.out = (nrow(.)))) 
-
-# Only add marker locations where we have an actual address
-locations <- curated %>%
-  filter(!is.na(address)) %>%
-  select(address)
-
-# Geocode addresses
-coords <- map_dfr(locations, geocode_OSM)
-
-# Create a dataframe of marker coordinates and labels
-markers <- curated %>%
-  inner_join(coords, by = c('address' = 'query')) %>%
-  mutate(label = paste(sep = "<br/>",
-                       sprintf("<b>%s</b>", id),
-                       business,
-                       address)) %>%
-  select(lat, lon, label)
+# 
+# 
+# # Only add marker locations where we have an actual address
+# locations <- curated %>%
+#   filter(!is.na(address)) %>%
+#   select(address)
+# 
+# # Geocode addresses
+# coords <- map_dfr(locations, geocode_OSM)
+# 
+# # Create a dataframe of marker coordinates and labels
+# markers <- curated %>%
+#   inner_join(coords, by = c('address' = 'query')) %>%
+#   mutate(label = paste(sep = "<br/>",
+#                        sprintf("<b>%s</b>", id),
+#                        business,
+#                        address)) %>%
+#   select(lat, lon, label)
+# 
+# hex <- c("#FF6663", "#FEB144", "#FDFD97", "#9EE09E", "#9EC1CF", "#995bc7", "#CC99C9", "#935218")
+# 
+# library(tmaptools)
+# 
+# curated <- responses %>% 
+#   row_to_names(1) %>% 
+#   select(id, contact_person, email, social_media, business, individual, 
+#          organisation, class, address, location, sector) %>% 
+#   arrange(id) 
+# 
+# busi <- curated %>% 
+#   filter(class=="A business") %>% 
+#   select(-organisation, -individual) %>% 
+#   rename(detail = 'business') %>% 
+#   rename(business='id') %>% 
+#   mutate(colours = rep_len(1:8, length.out = (nrow(.))))
+# 
+# orgs <- curated %>% 
+#   filter(class=="A NGO/NPO/Community Organisation") %>% 
+#   select(-business, -individual) %>% 
+#   rename(detail = 'organisation') %>% 
+#   rename(organisation='id') %>% 
+#   mutate(colours = rep_len(1:8, length.out = (nrow(.))))
+# 
+# indiv <- curated %>% 
+#   filter(class=="An individual") %>% 
+#   select(-business, -organisation, -address) %>% 
+#   rename(detail = 'individual') %>% 
+#   rename(individual='id') %>% 
+#   mutate(colours = rep_len(1:8, length.out = (nrow(.)))) 
+# 
+# # Only add marker locations where we have an actual address
+# locations <- curated %>%
+#   filter(!is.na(address)) %>%
+#   select(address)
+# 
+# # Geocode addresses
+# coords <- map_dfr(locations, geocode_OSM)
+# 
+# # Create a dataframe of marker coordinates and labels
+# markers <- curated %>%
+#   inner_join(coords, by = c('address' = 'query')) %>%
+#   mutate(label = paste(sep = "<br/>",
+#                        sprintf("<b>%s</b>", id),
+#                        business,
+#                        address)) %>%
+#   select(lat, lon, label)
 
 hex <- c("#FF6663", "#FEB144", "#FDFD97", "#9EE09E", "#9EC1CF", "#995bc7", "#CC99C9", "#935218")
 
