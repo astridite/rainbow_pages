c2ebe32f8859d43dd9a4f53e83ae1e86b028b394
astridite <astridite@gmail.com>
2020-09-18 17:19:57 +0200

    added app text to separate file

---
+++ RainbowPages/app_text.R
@@ -0,0 +1,25 @@
+hex <- c("#FF6663", "#FEB144", "#FDFD97", "#9EE09E", "#9EC1CF", "#995bc7", "#CC99C9", "#935218")
+
+sample_text <- "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+welcome_text <- "Hi! Welcome to the RainbowPages, a repository of all things openly and enthusiastically LGBTQIA+ in Cape Town, South Africa!"
+welcome_para1 <- "RainbowPages is a repository of businesses, organisations and individuals in Cape Town created to highlight and center the activities, connections and needs of people in the LGBTQIA+ community in our city."
+welcome_para2 <- "This is a passion project and relies on the input of volunteers. If you would like to contribute your time and skill, please get in touch!"
+
+business_text1 <- "LGBTQIA+ business listings in Cape Town:"
+
+organisation_text1 <- "Organisations in Cape Town that support LGBTQIA+ people:"
+
+individual_text1 <- "LGBTQIA+ entrepreneurs, professionals, artists and side-hustlers:"
+
+map_text1 <- "Explore LGBTQIA+ activities in the city:"
+
+work_in_progress <- "Rome was not built in a day, hunty. Check back in a few!"
+
+resources_text1 <- "Want some more information about the LGBTQIA+ experience in South Africa?"
+resources_text2 <- "Want to check out how this app was built using R Shiny?"
+
+signup_text1 <- "Want to see yourself in the RainbowPages? Sign up here:"
+signup_text2 <- "Responses will be added to the app in a few days."
+
+suggestion_text1 <- "We want to hear from you:"
