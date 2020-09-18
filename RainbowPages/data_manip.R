1ecd77624907c5602476c32f2c97119686ba2657
Matt Dennis <mattdenni@gmail.com>
2020-09-18 07:26:36 +0200

    Account for typo in 'counseling'

--- RainbowPages/data_manip.R
+++ RainbowPages/data_manip.R
@@ -36,7 +36,7 @@ curated <- responses %>%
   mutate(layer = str_replace_all(layer, "Film & Media|Photography", "Film, Media & Photography")) %>% 
   mutate(layer = str_replace_all(layer, "Food and Drink|Hospitality|Tourism","Hospitality & Tourism")) %>% 
   mutate(layer = str_replace_all(layer, "Community|Activism & Social Services","Community & Activism")) %>% 
-  mutate(layer = str_replace_all(layer, "Medical|Counselling","Medical & Counselling")) %>%
+  mutate(layer = str_replace_all(layer, "Medical|Counsel(l?)ing","Medical & Counselling")) %>%
   mutate(layer = str_replace_all(layer, "Fashion|Beauty","Fashion & Beauty")) %>%
   mutate(layer = str_replace_all(layer, "Marketing|Social Media","Marketing & Social Media")) %>%
   mutate(layer = str_replace_all(layer, "Manufacturing|Engineering","Manufacturing & Engineering")) %>%
