fa084bfe027372c68d4e990a66de433df689a9ba
astridite <astridite@gmail.com>
2020-09-19 16:29:51 +0200

    fleshed out "about section"

--- RainbowPages/app.R
+++ RainbowPages/app.R
@@ -11,110 +11,110 @@ library(here)
 source("app_text.R")
 
 curated_data <- read.csv(text=getURL("https://raw.githubusercontent.com/astridite/rainbow_pages/dev/RainbowPages/curated_data.csv")) 
-#markers <- read.csv(here("markers.csv"))
+
 
 ui <-  fluidPage(theme=shinytheme("simplex"),
                  setBackgroundColor("#FFF7FA"),
                  navbarPage(img(src="full_logo.png", height=28),
-                   #"Rainbow Pages Cape Town",
                             tabPanel("Browse",
-                          icon=icon("heart"),
-                          fluidPage(
-                            fluidRow(column(
-                              tags$h2(welcome_text), 
-                              tags$br(),
-                              tags$h4(welcome_para1, align="justify"),
-                              tags$br(),
-                              width = 11)
-                              ),
-                            sidebarLayout(
-                              sidebarPanel(
-                                tags$br(),
-                                style=HTML("background-color:  rgba(0, 0, 0, 0);", 
-                                           "border-color:  rgba(0, 0, 0, 0);",
-                                           "box-shadow: none;"),
-                                checkboxGroupInput('type', 
-                                                   label=tags$h4("Filter by Type"),
-                                                   selected=levels(factor(curated_data$class)),
-                                                   choices =levels(factor(curated_data$class)),
-                                                   inline = F),
-                                checkboxGroupInput('location', 
-                                            label=tags$h4("Filter by Location"),
-                                            selected=levels(factor(curated_data$location)),
-                                            choices =levels(factor(curated_data$location)),
-                                            inline = F,
-                                            width = '100%'),
-                                checkboxGroupInput('layer', 
-                                                   label=tags$h4("Filter by Activity"),
-                                                   selected=levels(factor(curated_data$layer)),
-                                                   choices =levels(factor(curated_data$layer)),
-                                                   inline = F,
-                                                   width = '100%'),
-                                width=2),
-                            mainPanel(
-                              (DT::dataTableOutput(outputId = "browse")))
-                          ))),
-             tabPanel("Map", icon=icon("map-marked-alt"),
-                      fluidRow(
-                               tags$br(),
-                               #column(tags$h3(map_text1), width=12),
-                               tags$head(
-                               tags$style(HTML("
-                               #controls {
-                               background-color: white;
-                               padding: 0 20px 20px 20px;
-                                               opacity: 0.5};"))),
-                               column(absolutePanel(id = "controls", fixed = TRUE,
-                                                    draggable = TRUE, top = 100, left = "auto", right = "auto", bottom = "auto",
-                                                    width = 230, height = "auto",
-                                                    style = "opacity: 0.8",
-                                                    
-                                                    checkboxGroupInput("category",
-                                                                       h2("Explore LGBTQIA+ Activities in Cape Town:"), 
-                                                                       choiceNames = levels(factor(curated_data$layer)),
-                                                                       choiceValues = levels(factor(curated_data$layer)),
-                                                                       selected = levels(factor(curated_data$layer)))), width=4),
-                               column(leafletOutput("map", height = 480, width=800), width=9, offset=3),
-                               
-                      )),
-             tabPanel("Sign-Up", icon=icon("user-plus"),
-                      fluidPage(column(
-                        tags$h3(signup_text1),
-                        tags$h5(signup_text2),
-                        tags$br(),
-                        tags$iframe(src="https://docs.google.com/forms/d/e/1FAIpQLSfp7dzCI7Pjh5H8RMEWvfh4_ba2ZbNoFSLfyuJlU6v4W4-nxg/viewform?usp=sf_link", width=700, height=500),
-                        width=8, offset=2))
-                      ), 
-             
-             tabPanel("Resources", icon=icon("info-circle"), 
-                          fluidPage(
-                            fluidRow(column(tags$h3(resources_text1,
-                                                    tags$a(href="https://www.sahistory.org.za/archive/life-orientation-classroom-sexuality-and-gender-pack?fbclid=IwAR3Thjx83Q4y4ntDa-78zPp-SFHuXKf_4uoMY55c1cLTRrwgp5DfYq36jUs",
-                                                           "Check out this comprehensive Gender and Sexuality Pack.")), width=8, offset=1),
-                                    column(tags$h3(resources_text2,
-                                                    tags$a(href="https://github.com/astridite/rainbow_pages",
-                                                           "Check out our Github Repo."), align='right'), width=8, offset=4))),
-                                    column(tags$h3(resources_text3, tags$a(href="mailto:rainbowpagesct@gmail.com","get in touch!")), width=8, offset=1),
-                                    column(tags$br(),tags$br(), tags$br(), width=12),
-                      
-                                    column(img(src="lettermark.png", height=200), width=4, offset=1)),
-                 # tabPanel("Support", icon=icon("hand-holding-usd"),
-                 #          fluidPage(
-                 #            fluidRow(column(tags$h3(work_in_progress), width=12)))),
-                 tabPanel("Suggestions", icon=icon("envelope-open-text"),
-                          fluidPage(column(
-                            tags$h3(suggestion_text1),
-                            tags$br(),
-                            tags$iframe(src=" https://docs.google.com/forms/d/e/1FAIpQLScNatTIO3l8PZNQK5QdrNlWuxHhCjfP7etHYt3HEo9rn97ztw/viewform?usp=sf_link", width=700, height=500),
-                            width=8, offset=2))
-                 )),
-             
-  setBackgroundImage(src="rp_background.png"),
-  tags$style(HTML("a {color: #eb34c0}")),
-  tags$style(type = 'text/css', '.navbar-default { background-color: rgba(0, 0, 0, 0);
-                                                   color: rgba(0, 0, 0, 0);}'),
-  tags$style(type = 'text/css',"navbar navbar-default navbar-static-top {color: rgba(0, 0, 0, 0);}")
-  )
+                                     icon=icon("heart"),
+                                     fluidPage(
+                                       fluidRow(column(
+                                         tags$h2(welcome_text), 
+                                         tags$br(),
+                                         tags$h4(welcome_para1, align="justify"),
+                                         tags$br(),
+                                         width = 11)
+                                         ),
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                           tags$br(),
+                                           style=HTML("background-color:  rgba(0, 0, 0, 0);", 
+                                                      "border-color:  rgba(0, 0, 0, 0);",
+                                                      "box-shadow: none;"),
+                                           checkboxGroupInput('type',
+                                                              label=tags$h4("Filter by Type"),
+                                                              selected=levels(factor(curated_data$class)),
+                                                              choices =levels(factor(curated_data$class)),
+                                                              inline = F),
+                                           checkboxGroupInput('location', 
+                                                              label=tags$h4("Filter by Location"),
+                                                              selected=levels(factor(curated_data$location)),
+                                                              choices =levels(factor(curated_data$location)),
+                                                              inline = F,
+                                                              width = '100%'),
+                                           checkboxGroupInput('layer', 
+                                                              label=tags$h4("Filter by Activity"),
+                                                              selected=levels(factor(curated_data$layer)),
+                                                              choices =levels(factor(curated_data$layer)),
+                                                              inline = F,
+                                                              width = '100%'),
+                                           width=2),
+                                         mainPanel(
+                                           (DT::dataTableOutput(outputId = "browse")))
+                                         ))),
+                            tabPanel("Map", icon=icon("map-marked-alt"),
+                                     fluidRow(
+                                       tags$br(),
+                                       #column(tags$h3(map_text1), width=12),
+                                       tags$head(
+                                       tags$style(HTML("
+                                       #controls {
+                                       background-color: white;
+                                       padding: 0 20px 20px 20px;
+                                                       opacity: 0.5};"))),
+                                       column(absolutePanel(id = "controls", fixed = TRUE,
+                                                            draggable = TRUE, top = 100, left = "auto", right = "auto", bottom = "auto",
+                                                            width = 230, height = "auto",
+                                                            style = "opacity: 0.8",
+                                                            checkboxGroupInput("category",
+                                                                               h4("Explore LGBTQIA+ Activities in Cape Town:"), 
+                                                                               choiceNames = levels(factor(curated_data$layer)),
+                                                                               choiceValues = levels(factor(curated_data$layer)),
+                                                                               selected = levels(factor(curated_data$layer)))), width=4),
+                                       column(leafletOutput("map", height = 480, width=800), width=9, offset=3)
+                                       )),
+                            tabPanel("Sign-Up", icon=icon("user-plus"),
+                                     fluidPage(column(
+                                       tags$h3(signup_text1),
+                                       tags$h5(signup_text2),
+                                       tags$br(),
+                                       tags$iframe(src="https://docs.google.com/forms/d/e/1FAIpQLSfp7dzCI7Pjh5H8RMEWvfh4_ba2ZbNoFSLfyuJlU6v4W4-nxg/viewform?usp=sf_link", width=700, height=400),
+                                       width=9, offset=2))
+                                     ),
+                            # tabPanel("Support", icon=icon("hand-holding-usd"),
+                            #          fluidPage(
+                            #            fluidRow(column(tags$h3(work_in_progress), width=12)))),
+                            tabPanel("Suggestions", icon=icon("envelope-open-text"),
+                                     fluidPage(column(
+                                       tags$h3(suggestion_text1),
+                                       tags$h5(suggestion_text2),
+                                       tags$br(),
+                                       tags$iframe(src=" https://docs.google.com/forms/d/e/1FAIpQLScNatTIO3l8PZNQK5QdrNlWuxHhCjfP7etHYt3HEo9rn97ztw/viewform?usp=sf_link", width=700, height=400),
+                                       width=9, offset=2))
+                                     ),
+                            tabPanel("About", icon=icon("info-circle"),
+                                     fluidPage(
+                                       fluidRow(column(tags$h3(about_text1,
+                                                               tags$a(href="https://www.sahistory.org.za/archive/life-orientation-classroom-sexuality-and-gender-pack?fbclid=IwAR3Thjx83Q4y4ntDa-78zPp-SFHuXKf_4uoMY55c1cLTRrwgp5DfYq36jUs",
+                                                                      "Check out this comprehensive Gender and Sexuality Pack.")), width=8, offset=1),
+                                                column(tags$h3(about_text2,
+                                                               tags$a(href="https://github.com/astridite/rainbow_pages",
+                                                                      "Check out our Github Repo."), align='right'), width=8, offset=4),
+                                                column(tags$h3(about_text3, tags$a(href="mailto:rainbowpagesct@gmail.com","get in touch!")), width=8, offset=1),
+                                                column(tags$br(),tags$br(), tags$br(), width=12)),
+                                       fluidRow(column(img(src="lettermark.png", height=200), width=3, offset=1), 
+                                                column(tags$h3(about_text4, tags$a(href="https://twitter.com/dataccino", "Astrid Radermacher"), "and", 
+                                                               tags$a(href="https://twitter.com/mattdenni", "Matt Dennis"), align='right'), 
+                                                       tags$h3(about_text5, tags$a(href="https://www.behance.net/liamlr95", "Liam Le Roux"), align='right'),
+                                                       width=6, offset=2))))
+                            ),
+                 setBackgroundImage(src="rp_background.png"),
+                 tags$style(HTML("a {color: #eb34c0}")),
+                 tags$style(type = 'text/css', '.navbar-default { background-color: rgba(0, 0, 0, 0);
+                            color: rgba(0, 0, 0, 0);}'),
+                 tags$style(type = 'text/css',"navbar navbar-default navbar-static-top {color: rgba(0, 0, 0, 0);}")
+                 )
 
 
 server <- function(input, output) {
--- RainbowPages/app_text.R
+++ RainbowPages/app_text.R
@@ -1,26 +1,26 @@
 hex <- c("#FF6663", "#FEB144", "#FDFD97", "#9EE09E", "#9EC1CF", "#995bc7", "#CC99C9", "#935218")
 
-rgb_cols <- c("rgb(255, 102, 99, 0.5)", "rgb(254, 177, 68, 0.5)", "rgb(253, 253, 151, 0.5)",
-              "rgb(158, 224, 158, 0.5)", "rgb(158, 193, 207, 0.5)", "rgb(153, 91, 199, 0.5)",
-              "rgb(204, 153, 201, 0.5)", "rgb(147, 82, 24, 0.5)")
+rgb_cols <- c("rgb(255, 102, 99, 0.6)", "rgb(254, 177, 68, 0.6)", "rgb(253, 253, 151, 0.6)",
+              "rgb(158, 224, 158, 0.6)", "rgb(158, 193, 207, 0.6)", "rgb(153, 91, 199, 0.6)",
+              "rgb(204, 153, 201, 0.6)", "rgb(147, 82, 24, 0.6)")
 
-sample_text <- "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 welcome_text <- "Hi! Welcome to the RainbowPages, a repository of all things openly and enthusiastically LGBTQIA+ in Cape Town, South Africa!"
 welcome_para1 <- "RainbowPages is a repository of businesses, organisations and individuals in Cape Town created to highlight and center the activities, connections and needs of people in the LGBTQIA+ community in our city."
-
-individual_text1 <- "LGBTQIA+ entrepreneurs, professionals, artists and side-hustlers:"
 
 map_text1 <- "Explore LGBTQIA+ activities in the city:"
 
 work_in_progress <- "Rome was not built in a day, hunty. Check back in a few!"
 
-resources_text1 <- "Want some more information about the LGBTQIA+ experience in South Africa?"
-resources_text2 <- "Want to check out how this app was built using R Shiny?"
-resources_text3 <- "This is a passion project and relies on the input of volunteers. If you would like to contribute your time and skill, please "
+about_text1 <- "Want some more information about the LGBTQIA+ experience in South Africa?"
+about_text2 <- "Want to check out how this app was built using R Shiny?"
+about_text3 <- "RainbowPages is a passion project and relies on the input of volunteers. If you would like to contribute your time and skill, please "
+about_text4 <- "This app was created with love by "
+about_text5 <- "Design work was done by"
 
 
 signup_text1 <- "Want to see yourself in the RainbowPages? Sign up here:"
 signup_text2 <- "New sign-ups are added weekly."
 
 suggestion_text1 <- "We want to hear from you:"
+suggestion_text2 <- "Compliments, critiques, suggestions and requests welcome."
